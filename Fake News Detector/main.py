79f353516ee97ee5aa3120bb81fe6d67852e4993
Cesar Cruz <cesarc68439@berkeley.edu>
2024-05-22 10:57:57 -0700

    Added DataFrame

--- Fake News Detector/main.py
+++ Fake News Detector/main.py
@@ -1,4 +1,10 @@
 import pandas as pd
 import numpy as np
+from IPython.display import display
 
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+
+data = pd.read_csv('data/fake_or_real_news.csv')
+display(data)
